86b565f3da88ef806dda9e045ff7406876b17786
wanghaoxi3000 <wanghaoxi3000@163.com>
2025-01-06 18:14:19 +0800

    Update copyright author link

--- themes/next/components/ArticleCopyright.js
+++ themes/next/components/ArticleCopyright.js
@@ -13,7 +13,8 @@ export default function ArticleCopyright ({ author, url }) {
       <ul className="overflow-x-auto whitespace-nowrap text-sm dark:bg-gray-700 bg-gray-100 p-5 leading-8 border-l-2 border-blue-500">
         <li>
           <strong className='mr-2'>{locale.COMMON.AUTHOR}:</strong>
-          <Link href={'/about'} className="hover:underline">
+          {/* <Link href={'/about'} className="hover:underline"> */}
+          <Link href={'/'} className="hover:underline">
             {author}
           </Link>
         </li>
